48fb46bca3db06a113b30feb8bc671fa55a1db50
algenioi <ian.algenio@gmail.com>
2023-10-20 22:08:08 -0400

    fixed mvc approx

--- mvc_approximations.py
+++ mvc_approximations.py
@@ -1,39 +1,53 @@
 from graph import *
-from random import randrange
-import matplotlib as plt
+from random import *
+from copy import deepcopy
+import random
+import matplotlib.pyplot as plt
+import math
 
 #Approximation algorithm implementations
 
 def approx1(G):
-    graph = G
-    C = {}
-    max_deg = math.inf
-    for node in range (graph.number_of_nodes):
-        if (len(graph[node])) >= max_deg:
-            v = i
-            C[v] = graph.adjacent_nodes(v)
-            graph[v] = []
-            if is_vertex_cover(G,C):
-                return C
+    graph = deepcopy(G)
+    C = set()
+    while True:
+        max_deg = 0
+        for node in range(graph.number_of_nodes()):
+            if (len(graph.adjacent_nodes(node))) > max_deg:
+                v = node
+                max_deg = len(graph.adjacent_nodes(node))
+        C.add(v)
+        for incident_node in (graph.adjacent_nodes(v)):
+            graph.adjacent_nodes(v).remove(incident_node)
+            graph.adjacent_nodes(incident_node).remove(v)
+        if is_vertex_cover(G,C):
+            return C
 
 def approx2(G):
-    C = {}
+    C = set()
     while not is_vertex_cover(G,C):
-        v = randrange(G.number_of_nodes)
+        while True: 
+            v = random.choice(list(G.adj.keys()))
+            if G.adjacent_nodes(v) != []:
+                break
         if v not in C:
-            C[v] = G.adjacent_nodes(v)
+            C.add(v)
     return C
 
 def approx3(G):
-    graph = G
-    C = {}
+    graph = deepcopy(G)
+    C = set()
     while not is_vertex_cover(G,C):
-        u = randrange(graph.number_of_nodes)
-        v = graph[u][randrange(len(graph[u]))]
-        C[u] = graph.adjacent_nodes=(u)
-        C[v] = graph.adjacent_nodes(v)
-        graph[u] = []
-        graph[v] = []
+        while True:
+            u = random.choice(list(graph.adj.keys()))
+            if graph.adjacent_nodes(u) != []:
+                v = random.choice(list(graph.adjacent_nodes(u)))
+                break
+        C.add(u)
+        C.add(v)
+        del graph.adj[u]
+        if v in graph.adj:
+            del graph.adj[v]
     return C
 
 #For experiments
@@ -57,7 +71,38 @@ def create_random_graph(num, n, m):
             print(graphs[j].adjacent_nodes(k))
  """
 def main():
-    create_random_graph(1000,8,1)
+    graph_sizes = []
+    a1_performance = []
+    a2_performance = []
+    a3_performance = []
+    for i in range(1,30,5):
+        graph_sizes.append(i)
+        graphs = create_random_graph(1000,8,i)
+        sum_mvc = 0
+        sum1 = 0
+        sum2 = 0
+        sum3 = 0
+        for graph in graphs:
+            mvc = MVC(graph)
+            sum_mvc += len(mvc)
+            a1 = approx1(graph)
+            sum1 += len(a1)
+            a2 = approx2(graph)
+            sum2 += len(a2)
+            a3 = approx3(graph)
+            sum3 += len(a3)
+        a1_performance.append(sum1/sum_mvc)
+        a2_performance.append(sum2/sum_mvc)
+        a3_performance.append(sum3/sum_mvc)
+    plt.plot(graph_sizes, a1_performance)
+    plt.plot(graph_sizes, a2_performance)
+    plt.plot(graph_sizes, a3_performance)
+    plt.legend(["approx1 performance", "approx2 performance", "approx3 performance"])
+    plt.xlabel("Graph sizes")
+    plt.ylabel("Performance (approx# sum / mvc sum)")
+    plt.show()
+
+    
 
 if __name__ == "__main__":
     main()
