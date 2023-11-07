8d4562911398c01835e9ee7b63703101316ae62a
hatoumg <ghena.ali.hatoum2003@gmail.com>
2023-11-07 09:46:26 -0500

    Finished

--- exp2.py
+++ exp2.py
@@ -4,7 +4,7 @@ from graph import is_connected
 
 def main():
     v, p = [], []
-    t = probability(n=100, min=100, inc=5, e=300, r=1000, f=is_connected)
+    t = probability(n=100, min=100, inc=5, e=500, r=1000, f=is_connected)
     for key in t:
         v.append(key)
         p.append(t[key])
